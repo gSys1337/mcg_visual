5114e143287bb8f9fe2df206d774b85746b6e39e
Jan Cichosz <jan.cichosz@web.de>
2025-01-27 10:53:42 +0100

    prepare adding cards

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,14 @@
+mod card;
+
+#[cfg(target_arch = "wasm32")]
+use crate::log;
 use eframe::emath::Align;
 use eframe::Frame;
 use egui::{Context, Direction};
-#[cfg(target_arch="wasm32")]
-use crate::log;
 
 pub struct App {
-    cards_texture: Option<egui::TextureId>,
+    cards_path: Option<String>,
+    // file_dialog: egui_file_dialog::FileDialog,
     current_state: Anchor,
 }
 
@@ -18,38 +21,37 @@ enum Anchor {
 impl eframe::App for App {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let layout = egui::Layout::from_main_dir_and_cross_align(Direction::TopDown, Align::Center);
-            ui.with_layout(layout, |ui| {
-                match self.current_state {
-                    Anchor::Menu => {
-                        let start = egui::Button::new("Start Game");
-                        if ui.add(start).clicked() {
-                            #[cfg(target_arch="wasm32")]
-                            log("game started");
-                            self.current_state = Anchor::Game;
-                        };
-                        let settings = egui::Button::new("Settings");
-                        if ui.add(settings).clicked() {
-                            #[cfg(target_arch="wasm32")]
-                            log("configuring textures");
-                            self.current_state = Anchor::Settings;
-                        }
+            let layout =
+                egui::Layout::from_main_dir_and_cross_align(Direction::TopDown, Align::Center);
+            ui.with_layout(layout, |ui| match self.current_state {
+                Anchor::Menu => {
+                    let start = egui::Button::new("Start Game");
+                    if ui.add(start).clicked() {
+                        #[cfg(target_arch = "wasm32")]
+                        log("game started");
+                        self.current_state = Anchor::Game;
+                    };
+                    let settings = egui::Button::new("Settings");
+                    if ui.add(settings).clicked() {
+                        #[cfg(target_arch = "wasm32")]
+                        log("configuring textures");
+                        self.current_state = Anchor::Settings;
                     }
-                    Anchor::Game => {
-                        let back = egui::Button::new("Back");
-                        if ui.add(back).clicked() {
-                            #[cfg(target_arch="wasm32")]
-                            log("back to main menu");
-                            self.current_state = Anchor::Menu;
-                        }
+                }
+                Anchor::Game => {
+                    let back = egui::Button::new("Back");
+                    if ui.add(back).clicked() {
+                        #[cfg(target_arch = "wasm32")]
+                        log("back to main menu");
+                        self.current_state = Anchor::Menu;
                     }
-                    Anchor::Settings => {
-                        let back = egui::Button::new("Back");
-                        if ui.add(back).clicked() {
-                            #[cfg(target_arch="wasm32")]
-                            log("back to main menu");
-                            self.current_state = Anchor::Menu;
-                        }
+                }
+                Anchor::Settings => {
+                    let back = egui::Button::new("Back");
+                    if ui.add(back).clicked() {
+                        #[cfg(target_arch = "wasm32")]
+                        log("back to main menu");
+                        self.current_state = Anchor::Menu;
                     }
                 }
             });
@@ -61,8 +63,12 @@ impl App {
     pub fn new(cc: &eframe::CreationContext) -> Self {
         egui_extras::install_image_loaders(&cc.egui_ctx);
         crate::utils::set_panic_hook();
-        #[cfg(target_arch="wasm32")]
+        #[cfg(target_arch = "wasm32")]
         log("New App created.");
-        Self { cards_texture: None, current_state: Anchor::Menu }
+        Self {
+            cards_path: None,
+            current_state: Anchor::Menu,
+            // file_dialog: egui_file_dialog::FileDialog::new(),
+        }
     }
 }
