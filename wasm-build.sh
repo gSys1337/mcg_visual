4e3e463e01923347067fca767e684a0ee322cc39
Jan Cichosz <jan.cichosz@web.de>
2025-02-06 13:19:44 +0100

    more verbose explanation

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# wasm_js
+# wasm_js; if rand crate fixes its dependencies, more specific: if it doesn't depend on getrandom <0.3
 #/bin/bash -l -c "export PATH=\"/home/janc/.cargo/bin:$PATH\" && cd /home/janc/mcg-visual && export RUSTFLAGS='--cfg getrandom_backend=\"wasm_js\"' && /home/janc/.cargo/bin/wasm-pack build --target web -- '--color=always'"
-# js
+# js; if get random 0.2.15 is dependency
 /bin/bash -l -c "export PATH=\"/home/janc/.cargo/bin:$PATH\" && cd /home/janc/mcg-visual && /home/janc/.cargo/bin/wasm-pack build --target web -- '--color=always'"
